92988e688b182407faaa16642b922e8b1a4e6e2f
Rainer Larin <rlarin@users.noreply.github.com>
2022-12-29 10:55:17 +0100

    :technologist: Section 13.56 Adding modifiers

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,7 +4,19 @@ pragma solidity >=0.4.22 <0.9.0;
 contract FaucetContract {
     // Storage variables
     uint public numOfFunders;
-    mapping(uint => address) private funders;
+    mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
+
+    // Modifiers
+    modifier onlyFunders() {
+        require(funders[msg.sender], "You are not a funder");
+        _;
+    }
+
+    modifier limitWithdrawal(uint amount) {
+        require(amount <= address(this).balance, "Insufficient funds");
+        _;
+    }
 
     // Events
 
@@ -12,12 +24,23 @@ contract FaucetContract {
     function receiveFunds() public payable {}
 
     function addFunds() external payable {
-        uint index = numOfFunders++;
-        funders[index] = msg.sender;
+        address funder = msg.sender;
+        if (!funders[funder]) {
+            funders[funder] = true;
+            lutFunders[numOfFunders++] = msg.sender;
+        }
+    }
+
+    function withdrawFunds(uint amount) external  limitWithdrawal(amount) {
+        payable(msg.sender).transfer(amount);
     }
 
     function getAllFunders() public view returns (address[] memory) {
-        return funders;
+        address[] memory _funders = new address[](numOfFunders);
+        for (uint i = 0; i < numOfFunders; i++) {
+            _funders[i] = funders[i];
+        }
+        return _funders;
     }
 
     function getFunderAtIndex(uint8 index) external view returns (address) {
